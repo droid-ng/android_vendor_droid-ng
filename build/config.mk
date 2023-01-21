86baa33dc66a8de389447fac8c2f81a4cb6f6a7d
nift4 <nift4@protonmail.com>
2023-01-21 12:49:18 +0100

    add overlays (settings rebrand, icon pack)

--- build/config.mk
+++ build/config.mk
@@ -2,3 +2,12 @@
 
 PRODUCT_EXTRA_RECOVERY_KEYS += \
     vendor/droid-ng/build/security/ng
+
+PRODUCT_ENFORCE_RRO_EXCLUDED_OVERLAYS += \
+    vendor/droid-ng/overlay/branding \
+    vendor/droid-ng/overlay/translations
+
+PRODUCT_PACKAGE_OVERLAYS += \
+    vendor/droid-ng/overlay/branding \
+    vendor/droid-ng/overlay/common \
+    vendor/droid-ng/overlay/translations
