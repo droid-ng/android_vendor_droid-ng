8d3adfcd0a18276b34f75066037d05ed533a1253
nift4 <nift4@protonmail.com>
2023-01-21 13:48:54 +0100

    add back wallpapers

--- build/config.mk
+++ build/config.mk
@@ -5,9 +5,17 @@ PRODUCT_EXTRA_RECOVERY_KEYS += \
 
 PRODUCT_ENFORCE_RRO_EXCLUDED_OVERLAYS += \
     vendor/droid-ng/overlay/branding \
-    vendor/droid-ng/overlay/translations
+    vendor/droid-ng/overlay/translations \
+    vendor/droid-ng/overlay/wallpapers_1080p \
+    vendor/droid-ng/overlay/wallpapers_1440p
 
 PRODUCT_PACKAGE_OVERLAYS += \
     vendor/droid-ng/overlay/branding \
     vendor/droid-ng/overlay/common \
     vendor/droid-ng/overlay/translations
+
+ifeq ($(shell test $(TARGET_SCREEN_WIDTH) -gt 1080; echo $$?),0)
+PRODUCT_PACKAGE_OVERLAYS += vendor/droid-ng/overlay/wallpapers_1440p
+else
+PRODUCT_PACKAGE_OVERLAYS += vendor/droid-ng/overlay/wallpapers_1080p
+endif
